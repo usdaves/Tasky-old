78ce57642fe9f3f5c300ed0d5e5d193be1d092ea
Usmon Abdurakhmanov <usdaves@gmail.com>
2023-02-22 16:07:31 +0500

    ♻️ `Result<T>` wrapper class, and her utilities created

---
+++ common/core/src/main/java/io/usdaves/core/util/ResultUtil.kt
@@ -0,0 +1,53 @@
+@file:OptIn(ExperimentalContracts::class)
+
+package io.usdaves.core.util
+
+import io.usdaves.core.Result
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+
+// Created by usdaves(Usmon Abdurakhmanov) on 2/22/2023
+
+inline fun <T> Result<T>.onEach(block: Result<T>.() -> Unit) = also(block)
+
+inline fun <T> Result<T>.onSuccess(block: Result.Success<T>.() -> Unit) = onEach {
+  if (this is Result.Success) block(this)
+}
+
+inline fun <T> Result<T>.onFailure(block: Result.Failure.() -> Unit) = onEach {
+  if (this is Result.Failure) block()
+}
+
+inline fun <T, R> Result<T>.map(transform: (T) -> R): Result<R> = when (this) {
+  is Result.Success -> Result.success(transform(data))
+  is Result.Failure -> Result.failure(throwable)
+}
+
+fun <T> Flow<T>.asResultFlow(): Flow<Result<T>> = map { data -> Result.success(data) }
+  .catch { throwable -> emit(Result.failure(throwable)) }
+
+inline fun <T> resultOf(block: () -> T): Result<T> = try {
+  Result.success(block())
+} catch (cancellation: CancellationException) {
+  throw cancellation
+} catch (throwable: Throwable) {
+  Result.failure(throwable)
+}
+
+fun Result<*>.isSuccess(): Boolean {
+  contract {
+    returns() implies (this@isSuccess is Result.Success<*>)
+  }
+  return this is Result.Success<*>
+}
+
+fun Result<*>.isFailure(): Boolean {
+  contract {
+    returns() implies (this@isFailure is Result.Failure)
+  }
+  return this is Result.Failure
+}
