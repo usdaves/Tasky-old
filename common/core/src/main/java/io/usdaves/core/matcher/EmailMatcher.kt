55ca2973716a4ed44c70f14e18e5e556d4ab817a
Usmon Abdurakhmanov <usdaves@gmail.com>
2023-02-23 10:05:44 +0500

    🔥 `Common/Core` module deleted

--- common/core/src/main/java/io/usdaves/core/matcher/EmailMatcher.kt
+++
@@ -1,8 +0,0 @@
-package io.usdaves.core.matcher
-
-// Created by usdaves(Usmon Abdurakhmanov) on 2/4/2023
-
-interface EmailMatcher {
-
-  fun matches(emailAddress: CharSequence): Boolean
-}
--- common/core/src/main/java/io/usdaves/core/remote/UserProfile.kt
+++
@@ -1,8 +0,0 @@
-package io.usdaves.core.remote
-
-// Created by usdaves(Usmon Abdurakhmanov) on 2/18/2023
-
-internal data class UserProfile(
-  val displayName: String?,
-  val email: String?,
-)
