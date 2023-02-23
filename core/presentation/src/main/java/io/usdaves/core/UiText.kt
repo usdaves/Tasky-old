a51bdb95ea97b3c5faf7660405d310e2614e92d1
Usmon Abdurakhmanov <usdaves@gmail.com>
2023-02-23 18:22:16 +0500

    ♻️ Arguments added to UiText class

--- core/presentation/src/main/java/io/usdaves/core/UiText.kt
+++ core/presentation/src/main/java/io/usdaves/core/UiText.kt
@@ -7,17 +7,16 @@ import java.io.Serializable
 // Created by usdaves(Usmon Abdurakhmanov) on 2/17/2023
 
 sealed class UiText : Serializable {
-
-  data class StringResource internal constructor(@StringRes val resId: Int) : UiText()
-  data class StringValue internal constructor(val value: String) : UiText()
+  private class Resource(@StringRes val resId: Int, vararg val args: Any) : UiText()
+  private class Value(val value: String) : UiText()
 
   fun asString(context: Context): String = when (this) {
-    is StringValue -> value
-    is StringResource -> context.getString(resId)
+    is Value -> value
+    is Resource -> context.getString(resId, args)
   }
 
   companion object {
-    operator fun invoke(@StringRes resId: Int): UiText = StringResource(resId)
-    operator fun invoke(value: String): UiText = StringValue(value)
+    operator fun invoke(@StringRes resId: Int, vararg args: Any): UiText = Resource(resId, args)
+    operator fun invoke(value: String): UiText = Value(value)
   }
 }
