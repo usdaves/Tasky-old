45ab843afa9244dd55ac9cdee50b94b1785b4e47
Usmon Abdurakhmanov <usdaves@gmail.com>
2023-02-24 21:45:05 +0500

    ✅ Unit tests created for SignUpUseCase, and SignInUseCase classes

---
+++ auth/domain/src/test/java/io/usdaves/auth/usecase/SignInUseCaseTest.kt
@@ -0,0 +1,88 @@
+package io.usdaves.auth.usecase
+
+import com.google.common.truth.Truth.assertThat
+import io.usdaves.auth.fake.FakeAuthRepository
+import io.usdaves.auth.fake.FakeEmailMatcher
+import io.usdaves.auth.fake.FakeLogger
+import io.usdaves.auth.repository.SignInResult
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+// Created by usdaves(Usmon Abdurakhmanov) on 2/24/2023
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class SignInUseCaseTest {
+
+  private lateinit var signInUseCase: SignInUseCase
+  private lateinit var emailMatcher: FakeEmailMatcher
+
+  private val defaultEmail = "email@mail.com"
+  private val defaultPassword = "password"
+
+  @Before
+  fun setUp() {
+    emailMatcher = FakeEmailMatcher()
+    signInUseCase = SignInUseCase(
+      repository = FakeAuthRepository(),
+      emailMatcher = emailMatcher,
+      logger = FakeLogger(),
+    )
+  }
+
+  @Test
+  fun `Empty email, Returns EmptyEmail result`() = runTest {
+    val result = signInUseCase(
+      email = "",
+      password = defaultPassword,
+    )
+    assertThat(result).isEqualTo(SignInResult.EmptyEmail)
+  }
+
+  @Test
+  fun `Empty password, Returns EmptyPassword result`() = runTest {
+    val result = signInUseCase(
+      email = defaultEmail,
+      password = "",
+    )
+    assertThat(result).isEqualTo(SignInResult.EmptyPassword)
+  }
+
+  @Test
+  fun `Invalid email, Returns InvalidCredentials result`() = runTest {
+    emailMatcher.setValidationResult(isEmailValid = false)
+    val result = signInUseCase(
+      email = "huntermailme",
+      password = defaultPassword,
+    )
+    assertThat(result).isEqualTo(SignInResult.InvalidCredentials)
+  }
+
+  @Test
+  fun `Too short password, Imminently returns InvalidCredentials result`() = runTest {
+    val result = signInUseCase(
+      email = defaultEmail,
+      password = "a".repeat(SignInUseCase.MIN_PASSWORD_LENGTH - 1),
+    )
+    assertThat(result).isEqualTo(SignInResult.InvalidCredentials)
+  }
+
+  @Test
+  fun `Too long password, Imminently returns InvalidCredentials result`() = runTest {
+    val result = signInUseCase(
+      email = defaultEmail,
+      password = "a".repeat(SignInUseCase.MAX_PASSWORD_LENGTH + 1),
+    )
+    assertThat(result).isEqualTo(SignInResult.InvalidCredentials)
+  }
+
+  @Test
+  fun `Valid email and password, Returns Success result`() = runTest {
+    val result = signInUseCase(
+      email = defaultEmail,
+      password = defaultPassword,
+    )
+    assertThat(result).isEqualTo(SignInResult.Success)
+  }
+}
---
+++ auth/domain/src/test/java/io/usdaves/auth/usecase/SignUpUseCaseTest.kt
@@ -0,0 +1,125 @@
+package io.usdaves.auth.usecase
+
+import com.google.common.truth.Truth.assertThat
+import io.usdaves.auth.fake.FakeAuthRepository
+import io.usdaves.auth.fake.FakeEmailMatcher
+import io.usdaves.auth.fake.FakeLogger
+import io.usdaves.auth.repository.SignUpResult
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+// Created by usdaves(Usmon Abdurakhmanov) on 2/24/2023
+
+@OptIn(ExperimentalCoroutinesApi::class)
+internal class SignUpUseCaseTest {
+
+  private lateinit var signUpUseCase: SignUpUseCase
+  private lateinit var emailMatcher: FakeEmailMatcher
+
+  private val defaultDisplayName = "DisplayName"
+  private val defaultEmail = "email@mail.com"
+  private val defaultPassword = "password"
+
+  @Before
+  fun setUp() {
+    emailMatcher = FakeEmailMatcher()
+    signUpUseCase = SignUpUseCase(
+      repository = FakeAuthRepository(),
+      emailMatcher = emailMatcher,
+      logger = FakeLogger(),
+    )
+  }
+
+  @Test
+  fun `Empty display name, Returns EmptyDisplayName result`() = runTest {
+    val result = signUpUseCase(
+      displayName = "",
+      email = defaultEmail,
+      password = defaultPassword,
+    )
+    assertThat(result).isEqualTo(SignUpResult.EmptyDisplayName)
+  }
+
+  @Test
+  fun `Empty email, Returns EmptyEmail result`() = runTest {
+    val result = signUpUseCase(
+      displayName = defaultDisplayName,
+      email = "",
+      password = defaultPassword,
+    )
+    assertThat(result).isEqualTo(SignUpResult.EmptyEmail)
+  }
+
+  @Test
+  fun `Empty password, Returns EmptyPassword result`() = runTest {
+    val result = signUpUseCase(
+      displayName = defaultDisplayName,
+      email = defaultEmail,
+      password = "",
+    )
+    assertThat(result).isEqualTo(SignUpResult.EmptyPassword)
+  }
+
+  @Test
+  fun `Too short display name, Returns TooShortDisplayName result`() = runTest {
+    val result = signUpUseCase(
+      displayName = "a".repeat(SignUpUseCase.MIN_DISPLAY_NAME_LENGTH - 1),
+      email = defaultEmail,
+      password = defaultPassword,
+    )
+    assertThat(result).isEqualTo(SignUpResult.TooShortDisplayName)
+  }
+
+  @Test
+  fun `Too long display name, Returns TooShortDisplayName result`() = runTest {
+    val result = signUpUseCase(
+      displayName = "a".repeat(SignUpUseCase.MAX_DISPLAY_NAME_LENGTH + 1),
+      email = defaultEmail,
+      password = defaultPassword,
+    )
+    assertThat(result).isEqualTo(SignUpResult.TooLongDisplayName)
+  }
+
+  @Test
+  fun `Invalid email, Returns InvalidEmail result`() = runTest {
+    emailMatcher.setValidationResult(isEmailValid = false)
+    val result = signUpUseCase(
+      displayName = "emailmailcom",
+      email = defaultEmail,
+      password = defaultPassword,
+    )
+    assertThat(result).isEqualTo(SignUpResult.InvalidEmail)
+  }
+
+  @Test
+  fun `Too short password, Returns TooShortPassword result`() = runTest {
+    val result = signUpUseCase(
+      displayName = defaultDisplayName,
+      email = defaultEmail,
+      password = "a".repeat(SignUpUseCase.MIN_PASSWORD_LENGTH - 1),
+    )
+    assertThat(result).isEqualTo(SignUpResult.TooShortPassword)
+  }
+
+  @Test
+  fun `Too long password, Returns TooShortPassword result`() = runTest {
+    val result = signUpUseCase(
+      displayName = defaultDisplayName,
+      email = defaultEmail,
+      password = "a".repeat(SignUpUseCase.MAX_PASSWORD_LENGTH + 1),
+    )
+    assertThat(result).isEqualTo(SignUpResult.TooLongPassword)
+  }
+
+  @Test
+  fun `Valid inputs, Returns Success result`() = runTest {
+    val result = signUpUseCase(
+      displayName = defaultDisplayName,
+      email = defaultEmail,
+      password = defaultPassword,
+    )
+    assertThat(result).isEqualTo(SignUpResult.Success)
+  }
+}
