6cc73ef15a84851e9fda97daa19d6dcecd3c98cd
Usmon Abdurakhmanov <usdaves@gmail.com>
2023-02-28 08:43:22 +0500

    ♻️ `equals()` and `hashCode()` functions implemented for `UiText` class

--- core/presentation/src/main/java/io/usdaves/core/UiText.kt
+++ core/presentation/src/main/java/io/usdaves/core/UiText.kt
@@ -7,8 +7,22 @@ import java.io.Serializable
 // Created by usdaves(Usmon Abdurakhmanov) on 2/17/2023
 
 sealed class UiText : Serializable {
-  private class Resource(@StringRes val resId: Int, vararg val args: Any) : UiText()
-  private class Value(val value: String) : UiText()
+
+  private data class Resource(@StringRes val resId: Int, val args: Array<out Any>) : UiText() {
+    override fun equals(other: Any?): Boolean {
+      if (this === other) return true
+      if (javaClass != other?.javaClass) return false
+      if (other !is Resource) return false
+      if (resId != other.resId) return false
+      return args.contentEquals(other.args)
+    }
+
+    override fun hashCode(): Int {
+      return 31 * resId + args.contentHashCode()
+    }
+  }
+
+  private data class Value(val value: String) : UiText()
 
   fun asString(context: Context): String = when (this) {
     is Value -> value
